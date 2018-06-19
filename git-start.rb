2c82c185c930375ccf759dac13b44a09a5936ea6
Yarbrough, Eric <eric_n_yarbrough@homedepot.com>
2018-06-19 00:26:59 -0500

    change sha256

--- git-start.rb
+++ git-start.rb
@@ -2,7 +2,7 @@ class GitStart < Formula
     desc "A git flow style tool that creates branches with Pivotal IDs in them"
     homepage "https://github.homedepot.com/ENY5JKO/git-start"
     url "https://github.homedepot.com/ENY5JKO/git-start/releases/download/0.0.1/git-start-0.0.1.tar.gz"
-    sha256 "efdb3c079c4ba5f2f817cd692f785cf40f3f910797d864b8b42ef5d46c5c2a18"
+    sha256 "1aef1148229c9655fbb8ef5d61eb78d50dc059f2eafab1c79b6bc3531129c7d5"
   
     bottle :unneeded
   
