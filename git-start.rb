a10928e4785b4c878cabc245287b824c95907e1d
Yarbrough, Eric <eric_n_yarbrough@homedepot.com>
2018-06-20 12:55:49 -0500

    change url to public github

--- git-start.rb
+++ git-start.rb
@@ -1,7 +1,7 @@
 class GitStart < Formula
     desc "A git flow style tool that creates branches with Pivotal IDs in them"
     homepage "https://github.homedepot.com/ENY5JKO/git-start"
-    url "https://github.homedepot.com/ENY5JKO/git-start/releases/download/0.0.1/git-start-0.0.1.tar.gz"
+    url "https://github.com/ericyarbrough/git-start/releases/download/v0.0.1/git-start-0.0.1.tar.gz"
     sha256 "5e2e5ac5406f7ddd3ee45bb4f3014aa04812001b288acedec17ff20229ecc06c"
   
     bottle :unneeded
