59ef2c1c44a5c2a42cc14f4424c6829f987192d3
Yarbrough, Eric <eric_n_yarbrough@homedepot.com>
2018-06-20 12:58:19 -0500

    change shasum

--- git-start.rb
+++ git-start.rb
@@ -2,7 +2,7 @@ class GitStart < Formula
     desc "A git flow style tool that creates branches with Pivotal IDs in them"
     homepage "https://github.homedepot.com/ENY5JKO/git-start"
     url "https://github.com/ericyarbrough/git-start/releases/download/v0.0.1/git-start-0.0.1.tar.gz"
-    sha256 "5e2e5ac5406f7ddd3ee45bb4f3014aa04812001b288acedec17ff20229ecc06c"
+    sha256 "e264ca6c063f4fa242fb8b02a0a6df34cdb22fb1d4dfd28aa94722fb9a05b1e7"
   
     bottle :unneeded
   
