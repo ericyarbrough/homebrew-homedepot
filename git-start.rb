b561bfa14021ba695d6845d06ede5b351852bf85
Yarbrough, Eric <eric_n_yarbrough@homedepot.com>
2018-06-22 22:11:42 -0400

    update to 0.0.2

--- git-start.rb
+++ git-start.rb
@@ -1,8 +1,8 @@
 class GitStart < Formula
     desc "A git flow style tool that creates branches with Pivotal IDs in them"
     homepage "https://github.com/ericyarbrough/git-start"
-    url "https://github.com/ericyarbrough/git-start/releases/download/v0.0.1/git-start.tar.gz"
-    sha256 "44422e134e3e8c8a3eaa134be5402df7f0ba3ca588907d852bccd36985c75b94"
+    url "https://github.com/ericyarbrough/git-start/releases/download/0.0.2/git-start-0.0.2.tar.gz"
+    sha256 "60f23c834d4d0d09cb1dc57c031842ca5fd7cf2aaf14f32b67b260913b99b99b"
   
     bottle :unneeded
   
